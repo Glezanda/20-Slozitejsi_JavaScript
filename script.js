ce7e5ee3000366d7d1b7829c36d7aad78aa8d833
Julia Melik <julija.melik@gmail.com>
2022-08-02 10:02:28 +0200

    one

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ colorV.style.color = " blue";
 let viText = document.querySelector(".vzdelani");
  viText.textContent = "Školy";
 
- let trida = document.querySelector(".tlacitko");
+ let trida = document.querySelector(".tlacitko1");
  trida.className= "zvyrazneny";
 
